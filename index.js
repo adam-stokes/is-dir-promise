d5f203a32395dae970cae5b04ce79599a665ecf1
Adam Stokes <adam.stokes@ubuntu.com>
2016-02-01 10:19:41 -0500

    Remove deprecated module
    Fixes: #1
    
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-"use strict";
+'use strict';
 
-var Promise = require("native-or-bluebird");
-var isDir = require("is-dir");
+const isDir = require('is-dir');
 
-module.exports = function (path) {
-    return new Promise(function(_resolve, _reject){
-        isDir(path, function(err, stats){
-            return err === null ? _resolve(stats) : _reject(err);
-        });
+module.exports = function(path) {
+  return new Promise(function(_resolve, _reject) {
+    isDir(path, function(err, stats) {
+      return err === null ? _resolve(stats) : _reject(err);
     });
+  });
 };
--- test.js
+++ test.js
@@ -5,16 +5,16 @@ var isDirP = require("./");
 require("should");
 
 describe("node module", function() {
-    it("should send back true for directory", function(done){
-        isDirP(__dirname).then(function(bool){
-            bool.should.be.true;
-            done();
-        });
+  it("should send back true for directory", function(done) {
+    isDirP(__dirname).then(function(bool) {
+      bool.should.be.true;
+      done();
     });
-    it("should catch errors", function(done){
-        isDirP("/aadfffdsafda").catch(function(err){
-            err.should.be.an.instanceOf(Error);
-            done();
-        });
+  });
+  it("should catch errors", function(done) {
+    isDirP("/aadfffdsafda").catch(function(err) {
+      err.should.be.an.instanceOf(Error);
+      done();
     });
+  });
 });
